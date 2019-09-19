fae9fe6faac8079b2ab2cf490fbf17496ce9c72d
mperrotta <mark_perrotta@hotmail.com>
2019-09-19 14:53:44 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/RickMoynihan/lein-tools-deps"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/tools.deps.alpha "0.7.549"]]
   ;:plugins [[thomasa/mranderson "0.4.8"]]
   :eval-in-leiningen true
