0cb07a14cba29f038bc8c2f6d4ebfe1831b209fd
mperrotta <mark_perrotta@hotmail.com>
2019-09-19 14:45:37 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/tools.deps.alpha "0.7.541"]]
+                 [org.clojure/tools.deps.alpha "0.7.505"]]
   ;:plugins [[thomasa/mranderson "0.4.8"]]
   :eval-in-leiningen true
   :min-lein-version "2.8.1")
